a368119586ca6d9716c8236b9d51b032eb1e39d2
Steven-Hazad <phanuth.hun@gmail.com>
2026-04-29 23:05:58 +0700

    Change name

--- src/app/page.js
+++ src/app/page.js
@@ -265,7 +265,7 @@ export default function Home() {
   }, []);
   const t = {
     en: {
-      name: "HUN PHANUTH Can Call me Steven",
+      name: "HUN PHANUTH",
       role: "System Architect",
       about: "About Me",
       aboutDesc: "I'm a student with a strong background in Computer Science and currently pursuing Air Traffic Management.",
